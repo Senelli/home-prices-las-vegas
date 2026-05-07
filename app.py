085c73d6471a9d374132f5985bf7a889308ff45c
Senelli <65837714+Senelli@users.noreply.github.com>
2026-05-06 22:11:33 -0700

    Update app.py
    resolving backend deployment issues on render for port

--- app.py
+++ app.py
@@ -75,4 +75,5 @@ def get_city_zipcodes():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
