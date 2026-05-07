d65e8487365d6f6bad4593f3d30d6edc00b63b15
Senelli <65837714+Senelli@users.noreply.github.com>
2026-05-06 22:31:48 -0700

    Update app.py
    corrected port

--- app.py
+++ app.py
@@ -75,5 +75,5 @@ def get_city_zipcodes():
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 1000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host='0.0.0.0', port=port, debug=False)
